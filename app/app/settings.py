198bfd301fd0ec1faba2e34bef98197fc1ec8e83
raphaellndr <raph.landure@gmail.com>
2025-02-18 15:09:55 +0100

    Organize installed apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third apps
     "drf_spectacular",
     "rest_framework",
     "rest_framework.authtoken",
+    # Local apps
     "core",
     "user",
     "sudoku",
