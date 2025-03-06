223f974a2d863b86bbe4e39f72b757bb5cc2ca06
raphaellndr <raph.landure@gmail.com>
2025-03-06 17:25:36 +0100

    Fix import

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -2,12 +2,13 @@
 
 from typing import Any
 
-from core.models import User
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
+
+from app.core.models import User
 
 CREATE_USER_URL = reverse("user:create")
 ME_URL = reverse("user:me")
