2f9b9f6ee168bff3a0dddce53905323a9154784f
raphaellndr <raph.landure@gmail.com>
2025-03-11 12:15:47 +0100

    Add autouse fixture to flush data after each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
+import pytest
+from django.core.management import call_command
+
 pytest_plugins = [
     "tests.plugins.factories.user",
     "tests.plugins.factories.sudoku",
     "tests.plugins.instances.clients",
     "tests.plugins.instances.payloads",
 ]
+
+
+@pytest.fixture(autouse=True)
+def db_flush_data(django_db_setup, django_db_blocker) -> None:
+    """Flushes data before after test."""
+    with django_db_blocker.unblock():
+        call_command("flush", "--no-input")
