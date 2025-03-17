55bf256821077ef4a9afa4a507abf8d7ffb7084e
raphaellndr <raph.landure@gmail.com>
2025-03-17 10:43:33 +0100

    Move different TextChoices to a separate module

---
+++ app/sudoku/choices.py
@@ -0,0 +1,24 @@
+"""Custom text choices."""
+
+from django.db.models import TextChoices
+from django.utils.translation import gettext_lazy as _
+
+
+class _ExtendedTextChoicesMeta(type(TextChoices)):
+    """Metaclass for `_ExtendedTextChoices` to dynamically compute `max_length` when defining
+    the class.
+    """
+
+    @property
+    def max_length(cls) -> int:
+        """Returns the maximum length."""
+        return max(len(value) for value in cls.values)
+
+
+class SudokuDifficultyChoices(TextChoices, metaclass=_ExtendedTextChoicesMeta):
+    """Sudoku difficulties enum."""
+
+    UNKNOWN = "Unknown", _("Unknown")
+    EASY = "Easy", _("Easy")
+    MEDIUM = "Medium", _("Medium")
+    HARD = "Hard", _("Hard")
--- app/sudoku/models.py
+++ app/sudoku/models.py
@@ -5,28 +5,22 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from .choices import SudokuDifficultyChoices
+
 
 class Sudoku(TimestampedMixin):
     """Sudoku object."""
 
-    class _DifficultyChoices(models.TextChoices):
-        """Sudoku difficulties enum."""
-
-        UNKNOWN = "Unknown", _("Unknown")
-        EASY = "Easy", _("Easy")
-        MEDIUM = "Medium", _("Medium")
-        HARD = "Hard", _("Hard")
-
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,  # remove every sudoku linked to the user
+        on_delete=models.CASCADE,
     )
     title = models.CharField(_("title"), max_length=255)
     difficulty = models.CharField(
         _("difficulty"),
-        max_length=10,
-        choices=_DifficultyChoices.choices,
-        default=_DifficultyChoices.UNKNOWN,
+        max_length=SudokuDifficultyChoices.max_length,
+        choices=SudokuDifficultyChoices.values,
+        default=SudokuDifficultyChoices.UNKNOWN,
     )
     grid = models.CharField(_("grid"), max_length=81, default="." * 81)
 
