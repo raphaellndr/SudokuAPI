1e5224c760ae407135e3ffea46ab1d518268db45
raphaellndr <raph.landure@gmail.com>
2025-03-25 16:25:32 +0100

    Check sudoku consistency before solving it

--- app/sudoku/tasks.py
+++ app/sudoku/tasks.py
@@ -17,6 +17,19 @@ def _update_sudoku_status(sudoku_id: str, status: SudokuStatusChoices) -> None:
     Sudoku.objects.filter(id=sudoku_id).update(status=status)
 
 
+def _check_consistency(sudoku_solver: SudokuResolver, /) -> bool:
+    """Checks if a sudoku is consistent or not.
+
+    :param sudoku_solver: Sudoku to check.
+    :returns: True if sudoku is consistent, False otherwise.
+    """
+    try:
+        sudoku_solver.check_consistency()
+        return True
+    except ConsistencyError:
+        return False
+
+
 @app.task
 def solve_sudoku(sudoku_id: str) -> dict[str, Any]:
     """Celery task to solve a Sudoku."""
@@ -26,15 +39,17 @@ def solve_sudoku(sudoku_id: str) -> dict[str, Any]:
 
         grid = copy(sudoku.grid)
         sudoku_solver = SudokuResolver(values=grid)
+
+        is_consistent = _check_consistency(sudoku_solver)
+        if not is_consistent:
+            _update_sudoku_status(sudoku_id, SudokuStatusChoices.INVALID)
+            return {"status": "failed", "error": "Inconsistent Sudoku"}
+
         sudoku_solver.solve()
 
-        try:
-            sudoku_solver.check_consistency()
-        except ConsistencyError:
-            _update_sudoku_status(
-                sudoku_id,
-                SudokuStatusChoices.INVALID,
-            )
+        is_consistent = _check_consistency(sudoku_solver)
+        if not is_consistent:
+            _update_sudoku_status(sudoku_id, SudokuStatusChoices.INVALID)
             return {"status": "failed", "error": "Inconsistent Sudoku solution"}
 
         solution_grid = sudoku_solver.to_string()
