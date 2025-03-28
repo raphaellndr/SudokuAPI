acca5911156ce40aa812cfe87c31412fef73fa92
raphaellndr <raph.landure@gmail.com>
2025-03-28 07:57:49 +0100

    Modify websocket_urlpatterns

--- app/sudoku/routing.py
+++ app/sudoku/routing.py
@@ -5,5 +5,5 @@ from django.urls import re_path
 from .consumers import SudokuStatusConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/sudoku/status/(?P<sudoku_id>[0-9a-fA-F-]+)/$", SudokuStatusConsumer.as_asgi())
+    re_path(r"ws/sudokus/(?P<sudoku_id>[0-9a-fA-F-]+)/status/$", SudokuStatusConsumer.as_asgi())
 ]
