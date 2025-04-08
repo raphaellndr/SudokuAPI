bc74c821d0e48ed9c0aa36ec95ee9112a72a5986
raphaellndr <raph.landure@gmail.com>
2025-04-08 15:57:36 +0200

    Tests serialization for both authenticated and anonymous users

--- tests/sudoku/test_serializers.py
+++ tests/sudoku/test_serializers.py
@@ -1,15 +1,33 @@
-from sudoku.models import SudokuSolution
-from sudoku.serializers import SudokuSerializer, SudokuSolutionSerializer
+"""Tests Sudoku serializers."""
+
+import pytest
+from sudoku.serializers import (
+    AnonymousSudokuSerializer,
+    SudokuSerializer,
+    SudokuSolutionSerializer,
+)
 
 
-def test_serialize_sudoku(create_user, create_sudoku) -> None:
-    """Tests that serializing a sudoku works as expected."""
-    user = create_user()
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "user",
+    [
+        "create_user",
+        None,
+    ],
+)
+def test_serialize_sudoku(request, create_sudoku, user: str | None) -> None:
+    """Tests that serializing a sudoku with and without a user works as expected."""
+    if user is not None:
+        user = request.getfixturevalue(user)()
     sudoku = create_sudoku(user=user)
-    serializer = SudokuSerializer(sudoku)
+
+    if user is not None:
+        serializer = SudokuSerializer(sudoku)
+    else:
+        serializer = AnonymousSudokuSerializer(sudoku)
 
     assert serializer.data["id"] == str(sudoku.id)
-    assert serializer.data["user_id"] == str(user.id)
     assert serializer.data["title"] == sudoku.title
     assert serializer.data["difficulty"] == sudoku.difficulty
     assert len(serializer.data["grid"]) == 81
@@ -18,19 +36,35 @@ def test_serialize_sudoku(create_user, create_sudoku) -> None:
     assert serializer.data["solution"] is None
     assert serializer.data["created_at"] == sudoku.created_at.isoformat().replace("+00:00", "Z")
     assert serializer.data["updated_at"] == sudoku.updated_at.isoformat().replace("+00:00", "Z")
+    if user is not None:
+        assert serializer.data["user_id"] == str(user.id)
+    else:
+        with pytest.raises(KeyError):
+            assert serializer.data["user_id"]
 
 
-def test_serialize_sudoku_solution(create_user, create_sudoku) -> None:
-    """Tests that serializing a sudoku solution works as expected."""
-    user = create_user()
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "user",
+    [
+        "create_user",
+        None,
+    ],
+)
+def test_serialize_sudoku_solution(
+    request, create_sudoku, create_sudoku_solution, user: str | None
+) -> None:
+    """Tests that serializing a sudoku solution with and without a user works as expected."""
+    if user is not None:
+        user = request.getfixturevalue(user)()
     sudoku = create_sudoku(user=user)
-    solution_grid = "8" * 81
-    sudoku_solution = SudokuSolution.objects.create(sudoku=sudoku, grid=solution_grid)
+
+    sudoku_solution = create_sudoku_solution(sudoku=sudoku)
     serializer = SudokuSolutionSerializer(sudoku_solution)
 
     assert serializer.data["id"] == str(sudoku_solution.id)
     assert serializer.data["sudoku_id"] == str(sudoku.id)
-    assert serializer.data["grid"] == solution_grid
+    assert sudoku_solution.grid
     assert serializer.data["created_at"] == sudoku_solution.created_at.isoformat().replace(
         "+00:00", "Z"
     )
@@ -39,16 +73,30 @@ def test_serialize_sudoku_solution(create_user, create_sudoku) -> None:
     )
 
 
-def test_serialize_sudoku_with_solution(create_user, create_sudoku) -> None:
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "user",
+    [
+        "create_user",
+        None,
+    ],
+)
+def test_serialize_sudoku_with_solution(
+    request, create_sudoku, create_sudoku_solution, user: str | None
+) -> None:
     """Tests that serializing a sudoku with a solution works as expected."""
-    user = create_user()
+    if user is not None:
+        user = request.getfixturevalue(user)()
     sudoku = create_sudoku(user=user)
-    solution_grid = "8" * 81
-    sudoku_solution = SudokuSolution.objects.create(sudoku=sudoku, grid=solution_grid)
-    serializer = SudokuSerializer(sudoku)
+
+    sudoku_solution = create_sudoku_solution(sudoku=sudoku)
+
+    if user is not None:
+        serializer = SudokuSerializer(sudoku)
+    else:
+        serializer = AnonymousSudokuSerializer(sudoku)
 
     assert serializer.data["id"] == str(sudoku.id)
-    assert serializer.data["user_id"] == str(user.id)
     assert serializer.data["title"] == sudoku.title
     assert serializer.data["difficulty"] == sudoku.difficulty
     assert len(serializer.data["grid"]) == 81
@@ -59,10 +107,15 @@ def test_serialize_sudoku_with_solution(create_user, create_sudoku) -> None:
 
     assert serializer.data["solution"]["id"] == str(sudoku_solution.id)
     assert serializer.data["solution"]["sudoku_id"] == str(sudoku.id)
-    assert serializer.data["solution"]["grid"] == solution_grid
+    assert serializer.data["solution"]["grid"]
     assert serializer.data["solution"][
         "created_at"
     ] == sudoku_solution.created_at.isoformat().replace("+00:00", "Z")
     assert serializer.data["solution"][
         "updated_at"
     ] == sudoku_solution.updated_at.isoformat().replace("+00:00", "Z")
+    if user is not None:
+        assert serializer.data["user_id"] == str(user.id)
+    else:
+        with pytest.raises(KeyError):
+            assert serializer.data["user_id"]
