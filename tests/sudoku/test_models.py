4d3a1e5d2977022b7d68bd99829f5fb34fbf8a0a
raphaellndr <raph.landure@gmail.com>
2025-04-08 15:55:39 +0200

    Modify tests to cover anonymous users; add test to assert that a sudoku cannot have more than one solution

--- tests/sudoku/test_models.py
+++ tests/sudoku/test_models.py
@@ -1,44 +1,83 @@
-"""Sudoku model tests."""
+"""Tests Sudoku models for both authenticated and anonymous users."""
 
 import pytest
 from django.db import IntegrityError
 from sudoku.choices import SudokuDifficultyChoices, SudokuStatusChoices
-from sudoku.models import Sudoku, SudokuSolution
+from sudoku.models import SudokuSolution
 
 
-def test_cannot_create_sudoku_without_user(transactional_db) -> None:
-    """Tests that creating a new sudoku without a user is not possible."""
-    with pytest.raises(IntegrityError):
-        Sudoku.objects.create(title="Test Sudoku")
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "user",
+    [
+        "create_user",
+        None,
+    ],
+)
+def test_create_sudoku(request, create_sudoku, user: str | None) -> None:
+    """Tests that creating a new sudoku is successful."""
+    if user is not None:
+        user = request.getfixturevalue(user)()
+    sudoku = create_sudoku(user=user)
 
-
-def test_create_sudoku(create_sudoku) -> None:
-    """Tests creating a new sudoku."""
-    sudoku = create_sudoku()
-
-    assert sudoku.user
+    assert sudoku.user == user
     assert sudoku.title == "sudoku title"
     assert sudoku.difficulty in SudokuDifficultyChoices.values
-    assert len(sudoku.grid) == 81
+    assert sudoku.grid
     assert sudoku.status in SudokuStatusChoices.CREATED
     assert str(sudoku) == f"Sudoku {sudoku.id} - Status: {sudoku.status}"
 
 
-def test_create_sudoku_solution(transactional_db) -> None:
+@pytest.mark.django_db
+def test_create_sudoku_solution_without_sudoku(create_sudoku_solution) -> None:
     """Tests that creating a new solution without a sudoku is not possible."""
     with pytest.raises(IntegrityError):
-        SudokuSolution.objects.create()
+        create_sudoku_solution(sudoku=None)
 
 
-def test_create_sudoku_with_solution(create_sudoku) -> None:
-    """Tests creating a new sudoku with a solution.
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "user",
+    [
+        "create_user",
+        None,
+    ],
+)
+def test_create_sudoku_with_solution(
+    request, create_sudoku, create_sudoku_solution, user: str | None
+) -> None:
+    """Tests creating a new sudoku with a solution is successful.
 
     Checks that the solution exists and that the related sudoku is the correct one.
     """
-    sudoku = create_sudoku()
-    solution_grid = "8" * 81
-    sudoku_solution = SudokuSolution.objects.create(sudoku=sudoku, grid=solution_grid)
+    if user is not None:
+        user = request.getfixturevalue(user)()
+    sudoku = create_sudoku(user=user)
 
-    assert sudoku.solution.grid == solution_grid
+    sudoku_solution = create_sudoku_solution(sudoku=sudoku)
+
+    assert sudoku.solution.grid
     assert sudoku_solution.sudoku.id == sudoku.id
     assert str(sudoku_solution) == f"Solution for Sudoku {sudoku.id}"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "user",
+    [
+        "create_user",
+        None,
+    ],
+)
+def test_create_sudoku_with_two_solution(
+    request, create_sudoku, create_sudoku_solution, user
+) -> None:
+    """Tests that creating a new sudoku with more than one solution is not possible."""
+    if user is not None:
+        user = request.getfixturevalue(user)()
+    sudoku = create_sudoku(user=user)
+
+    create_sudoku_solution(sudoku=sudoku)
+
+    with pytest.raises(IntegrityError):
+        SudokuSolution.objects.create(sudoku=sudoku)
