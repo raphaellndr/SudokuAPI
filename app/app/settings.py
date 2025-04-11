0dec83a03ed6cd0e3b0388ee6d90d46124287377
raphaellndr <raph.landure@gmail.com>
2025-04-11 09:23:15 +0200

    Fix channels config (set correct celery broker url)

--- app/app/settings.py
+++ app/app/settings.py
@@ -263,7 +263,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [env("CELERY_BROKER_URL")],
         },
     },
 }
