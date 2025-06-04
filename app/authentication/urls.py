c878d064ac8f44525adcfbc64e657ee9aea1af5b
raphaellndr <raph.landure@gmail.com>
2025-06-04 10:20:25 +0200

    Remove user/ endpoint

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -8,8 +8,6 @@ from django.urls import path
 from django.urls.resolvers import URLPattern, URLResolver
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from app.user.views import ManageUserView
-
 from . import views
 
 app_name: Final[str] = "authentication"
@@ -18,7 +16,6 @@ urlpatterns: list[URLResolver | URLPattern] = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("user/", ManageUserView.as_view(), name="rest_user_details"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
