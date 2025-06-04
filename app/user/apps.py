c9db86f70ffd4b17f77f6fc4d9751231310d5c19
raphaellndr <raph.landure@gmail.com>
2025-06-04 10:40:44 +0200

    Import signals when User app is ready

--- app/user/apps.py
+++ app/user/apps.py
@@ -8,3 +8,7 @@ class UserConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "app.user"
+
+    def ready(self):
+        """Import signals when app is ready."""
+        import app.user.signals  # noqa
